3d50c899dc0050d9820232eb49246a3b48cf094e
James Segarra III <JamesSegarra@gmail.com>
2018-05-01 16:46:12 -0400

    Annotations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  text        :string
+#  correct     :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Answer < ApplicationRecord
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  test_id    :integer
+#  text       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Question < ApplicationRecord
 end
